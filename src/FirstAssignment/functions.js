01ba57eb19ad62b32089ab985e72e2248d23936a
Patience <patience@ext.kobo360.com>
2021-07-30 10:18:27 -0500

    editted getFullNameInitials arguments

--- src/FirstAssignment/functions.js
+++ src/FirstAssignment/functions.js
@@ -1,21 +1,21 @@
-function getFullNameInitials(names) {
-    let fullNames = names;
+function getFullNameInitials(firstName, lastName) {
+    let fullNames = firstName.concat(lastName);
     // split() creates an array of strings when used on a string or object
     let newNames = fullNames.split(' '); 
     // shift() removes from the beginning of an array while slice removes from a string(what index, how many element should be removed)
     let firstNameInitial =  newNames.shift().slice(0,1); 
     //pop() removes from the end of an array
     let secondNameInitial = newNames.pop().slice(0,1);
-    let result = firstNameInitial + " " + secondNameInitial; 
+    let result = firstNameInitial + "" + secondNameInitial; 
     return result;
 }
 
 function getRandomNumBetweenMinAndMax(min, max) {
     // Math.ceil() rounds up a decimal number and returns an integer
-    min = Math.ceil(min);
+    const minMin = Math.ceil(min);
     // Math.floor() rounds down a decimal number and returns an integer
-    max = Math.floor(max);
-    return Math.floor(Math.random()*(max-min+1)+min);
+    const maxMax = Math.floor(max);
+    return Math.floor(Math.random()*(maxMax-minMin+1)+minMin);
 }
 
 
